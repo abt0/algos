0c04e55212897f924a6eef416f811796de49284f
Timur Abdumuminov <t.abdumuminov@traviangames.com>
2019-07-30 22:40:41 +0200

    Add desc/asc direction to Insertion sort

--- sorting/insertion-sort.py
+++ sorting/insertion-sort.py
@@ -1,9 +1,9 @@
-def insertion_sort(arr):
+def insertion_sort(arr, desc=False):
     for i, item in enumerate(arr):
         if i == 0:
             continue
         j = i - 1
-        while j >= 0 and arr[j] > item:
+        while j >= 0 and (arr[j] > item) ^ desc:
             arr[j + 1] = arr[j]
             j = j - 1
         arr[j + 1] = item
