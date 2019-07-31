3c58741e1b9e610591e045db280de505cfc76649
Timur Abdumuminov <t.abdumuminov@traviangames.com>
2019-07-31 08:15:55 +0200

    Add minor simplification Insertion sort

--- sorting/insertion-sort.py
+++ sorting/insertion-sort.py
@@ -5,6 +5,6 @@ def insertion_sort(arr, desc=False):
         j = i - 1
         while j >= 0 and (arr[j] > item) ^ desc:
             arr[j + 1] = arr[j]
-            j = j - 1
+            j -= 1
         arr[j + 1] = item
     return arr
