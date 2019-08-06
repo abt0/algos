87377fab3d749140e8c434d395114e07380e4834
Timur Abdumuminov <timur.abdumuminov@gmail.com>
2019-08-06 14:30:22 +0200

    Add arithmetic series implementation

---
+++ math/arithmetic-series.py
@@ -0,0 +1,21 @@
+# O(n^2)
+
+
+def arithmetic_series(n: int) -> int:
+    if n < 0:
+        raise ValueError('Arguments n is not a natural number')
+
+    return int((n + 1) * n * 0.5)
+
+
+def arithmetic_series_loop(n: int) -> int:
+    if n < 0:
+        raise ValueError('Arguments n is not a natural number')
+
+    s: int = 0
+
+    while n > 0:
+        s += n
+        n -= 1
+
+    return s
